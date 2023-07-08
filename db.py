a1b37fdd9557ab81ee6979be52f748d2256c0cd2
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-07-09 01:38:15 +0300

    mac start

--- db.py
+++ db.py
@@ -19,7 +19,20 @@ class database:
         self.db = await sql.connect('mydatabase.db')
         self.cur = await self.db.cursor()
         await self.cur.execute("""CREATE TABLE if not exists users (
-                    chatid TEXT
+                    chatid TEXT,
+                    balance TEXT,
+                    cart TEXT
+                    )""")
+        await self.cur.execute("""CREATE TABLE if not exists products (
+                    name TEXT,
+                    price TEXT,
+                    photo_path TEXT,
+                    description TEXT
+                    )""")
+        await self.cur.execute("""CREATE TABLE if not exists products_view (
+                    name TEXT,
+                    chat_id TEXT,
+                    view TEXT
                     )""")
         await self.db.commit()
 
@@ -28,7 +41,20 @@ class database:
             await self.cur.execute(""" SELECT * FROM users WHERE chatid = ? """, (user_id,))
             result = await self.cur.fetchall()
             if len(result) < 1:
-                await self.cur.execute(""" INSERT INTO users ("chatid") VALUES (?) """, (user_id,))
+                await self.cur.execute(""" INSERT INTO users ("chatid", "balance", "cart") VALUES (?,?,?) """, (user_id,"0", ";"))
             await self.db.commit()
             return {"status" : True}
         else: return {"status" : False}
+
+    async def insert_product(self, name : str = None, price : float = None, photo_path : str = None, description : str = None):
+        if name != None:
+            await self.cur.execute(""" SELECT * FROM products WHERE name = ? """, (name,))
+            result = await self.cur.fetchall()
+            if len(result) < 1:
+                if name and price and photo_path and description: await self.cur.execute(""" INSERT INTO products ("name", "price", "photo_path", "description", "views") VALUES (?,?,?,?,?) """, (name, price, photo_path, description, ";"))
+                else: return  {"status" : False, "error" : "Произошла ошибка, попробуйте снова"}
+            else: return  {"status" : False, "error" : "Товар с таким именем уже существует"}
+            await self.db.commit()
+            return {"status" : True}
+        else: return {"status" : False, "error" : "Произошла ошибка, попробуйте снова"}
+
